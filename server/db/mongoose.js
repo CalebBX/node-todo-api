6a3d98aa9e4e23b6c481d69cb59ad4740079fd94
CalebBX <cbrocomp@gmail.com>
2017-10-27 00:25:06 -0700

    fixed unhandled promise error for database server connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ,{useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI ,{useMongoClient: true})
+.catch(function(e){
+    console.log('Failed to connect to MongoDB server');
+});
 
 module.exports = {mongoose};
